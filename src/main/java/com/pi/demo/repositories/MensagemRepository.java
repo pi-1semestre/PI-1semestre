9b55aa47f7f6604a4b51efa4da52803958b3b6e9
pedrh-carmo <pedro.campos.1bmangal@gmail.com>
2026-06-09 20:02:02 -0300

    bugfix: correcoes tabela mensagem e fetchtype

--- src/main/java/com/pi/demo/repositories/MensagemRepository.java
+++ src/main/java/com/pi/demo/repositories/MensagemRepository.java
@@ -3,10 +3,19 @@ package com.pi.demo.repositories;
 import com.pi.demo.entities.Mensagem;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
     
     List<Mensagem> findByStatusTrue();
+    
+    List<Mensagem> findByStatusFalse();
+    
+    List<Mensagem> findByNomeAnimalContaining(String nome);
+
+    @Query("SELECT m FROM Mensagem m WHERE m.nomeAnimal = :nomeAnimal")
+    List<Mensagem> findByNomeAnimal(@Param("nomeAnimal") String nomeAnimal);
 }
